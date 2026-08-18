05f443c69d934487fa1ac2a8afa442a87858218c
Paraskevi Tsichlakidou <tsichlakidou@web.de>
2026-08-18 17:00:33 +0200

    product class

---
+++ products.py
@@ -0,0 +1,42 @@
+class Product:
+    def __init__(self, name, price, quantity):
+        if name == "":
+            raise Exception("Name cannot be empty")
+        if price < 0:
+            raise Exception("Price cannot be negative")
+        if quantity < 0:
+            raise Exception("Quantity cannot be negative")
+
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.active = True
+
+    def get_quantity(self)-> int:
+        return self.quantity
+
+    def set_quantity(self, quantity):
+        if self.quantity <=0:
+            deactivate(self)
+
+    def is_active(self) -> bool:
+        return self.active
+
+    def activate(self) -> bool:
+        self.active = True
+
+    def deactivate(self):
+        self.active = False
+
+    def show(self):
+        print(self.name, "Price:",self.price, "Quantity:", self.quantity)
+
+    def buy(self, quantity) -> float:
+        if quantity <= 0:
+            raise Exception("Quantity must be positive")
+
+        if quantity > self.quantity:
+            raise Exception("There are not enough products for this purchase")
+
+        self.quantity -= quantity
+        return self.price * quantity
