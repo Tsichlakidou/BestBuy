7d22dd1e474003fb8de5a2edb45541f3400ae859
Paraskevi Tsichlakidou <tsichlakidou@web.de>
2026-08-19 10:08:52 +0200

    all set

--- main.py
+++ main.py
@@ -1,14 +1,73 @@
-from products import Product
+import products
+import store
 
-bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
-mac = Product("MacBook Air M2", price=1450, quantity=100)
 
-print(bose.buy(50))
-print(mac.buy(100))
-print(mac.is_active())
+# Setup initial stock of inventory
+product_list = [
+    products.Product("MacBook Air M2", price=1450, quantity=100),
+    products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+    products.Product("Google Pixel 7", price=500, quantity=250)
+]
 
-bose.show()
-mac.show()
+best_buy = store.Store(product_list)
 
-bose.set_quantity(1000)
-bose.show()
+
+def start(store):
+    while True:
+        print("\nStore Menu")
+        print("----------")
+        print("1. List all products in store")
+        print("2. Show total amount in store")
+        print("3. Make an order")
+        print("4. Quit")
+
+        choice = input("Please choose a number: ")
+
+        if choice == "1":
+            all_products = store.get_all_products()
+
+            for product in all_products:
+                product.show()
+
+        elif choice == "2":
+            total_quantity = store.get_total_quantity()
+            print("Total of", total_quantity, "items in store")
+
+        elif choice == "3":
+            shopping_list = []
+
+            all_products = store.get_all_products()
+
+            for index, product in enumerate(all_products, start=1):
+                print(index, end=". ")
+                product.show()
+
+            while True:
+                product_number = input(
+                    "Which product do you want? Enter # or press Enter to finish: "
+                )
+
+                if product_number == "":
+                    break
+
+                quantity = int(input("What amount do you want? "))
+
+                product = all_products[int(product_number) - 1]
+                shopping_list.append((product, quantity))
+
+            try:
+                total_price = store.order(shopping_list)
+                print("Order made! Total payment:", total_price)
+
+            except Exception as e:
+                print("Error:", e)
+
+        elif choice == "4":
+            print("Goodbye!")
+            break
+
+        else:
+            print("Invalid choice.")
+
+
+start(best_buy)
---
+++ store.py
@@ -0,0 +1,33 @@
+from itertools import product
+
+import products
+
+
+class Store:
+    def __init__(self,products):
+        self.products = products
+
+    def add_product(self, product):
+        self.products.append(product)
+
+    def remove_product(self, product):
+        self.products.remove(product)
+
+    def get_total_quantity(self)->int:
+        return sum([product.quantity for product in self.products])
+
+    def get_all_products(self)->List[Product]:
+        active_products = []
+        for product in self.products:
+            if product.is_active():
+                active_products.append(product)
+        return active_products
+
+    def order(self, shopping_list)-> float:
+        total_price = 0
+        for product, quantity in shopping_list:
+            total_price += product.buy(quantity)
+        return total_price
+
+
+
